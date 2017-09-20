e8ca44c8f24fb7700f4834a4218aa00f94d9e6cf
carolmorneau <carol.morneau@solace.com>
2017-09-20 18:17:31 +0000

    Installing cf-uaac before using it

--- bin/pcf_prepare.sh
+++ bin/pcf_prepare.sh
@@ -49,6 +49,7 @@ function addBuildPack() {
 
 
 function enableTcpRoutingForSolaceRouter() {
+        gem install cf-uaac
 
 	TARGET=${1:-"uaa.local.pcfdev.io"}
 	uaac target $TARGET --skip-ssl-validation
