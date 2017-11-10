da9e641b88890716e6d4b71440e61b0e1088e966
Ubuntu <ubuntu@solace-messaging-cf-dev.cli-tools>
2017-11-10 21:23:54 +0000

    .profile to rely on bosh-common.sh, shorten bosh cmd using global BOSH_* envs

--- bin/bosh-common.sh
+++ bin/bosh-common.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export MY_BIN_HOME=$(dirname $(readlink -f $0))
+export MY_BIN_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 export PYTHONPATH=$MY_BIN_HOME
-export MY_HOME=$MY_BIN_HOME/..
 
 export DEPLOYMENT_NAME=${DEPLOYMENT_NAME:-"solace-vmr-warden-deployment"}
 export LOG_FILE=${LOG_FILE:-"/tmp/bosh_deploy.log"}
@@ -18,22 +18,39 @@ export USE_ERRANDS=${USE_ERRANDS:-"1"}
 
 ######################################
 
+export BOSH_IP="192.168.50.4"
 export BOSH_CMD="/usr/local/bin/bosh"
 export BOSH_CLIENT=${BOSH_CLIENT:-admin}
 export BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET:-admin}
 export BOSH_NON_INTERACTIVE${BOSH_NON_INTERACTIVE:-true}
+export BOSH_ENVIRONMENT=lite
+export BOSH_DEPLOYMENT=$DEPLOYMENT_NAME
+
 
 function targetBosh() {
   
-  if [ ! -d $HOME/bosh-lite ]; then
-     (cd $HOME; git clone https://github.com/cloudfoundry/bosh-lite.git)
+  if [ ! -d $WORKSPACE/bosh-lite ]; then
+     (cd $WORKSPACE; git clone https://github.com/cloudfoundry/bosh-lite.git)
   fi
 
- # bosh target 192.168.50.4 lite
-  $BOSH_CMD -e 192.168.50.4 --ca-cert=~/bosh-lite/ca/certs/ca.crt --client=admin --client-secret=admin alias-env lite
-  BOSH_CLIENT=$BOSH_CLIENT BOSH_CLIENT_SECRET=$BOSH_CLIENT_SECRET $BOSH_CMD -e lite log-in
+  # bosh target $BOSH_IP alias as 'lite'
+  BOSH_TARGET_LOG=$( $BOSH_CMD alias-env lite -e $BOSH_IP --ca-cert=$WORKSPACE/bosh-lite/ca/certs/ca.crt --client=admin --client-secret=admin  )
+  if [ $? -eq 0 ]; then
+    # Login will rely on BOSH_* env vars..
+    BOSH_LOGIN_LOG=$( BOSH_CLIENT=$BOSH_CLIENT BOSH_CLIENT_SECRET=$BOSH_CLIENT_SECRET $BOSH_CMD log-in )
+    if [ $? -eq 0 ]; then
+       export BOSHLITE=1
+    else
+       export BOSHLITE=0
+       echo $BOSH_LOGIN_LOG
+    fi
+  else
+     export BOSHLITE=0
+     echo $BOSH_TARGET_LOG
+  fi
 
 }
+
 
 function prepareBosh() {
 
@@ -41,23 +58,21 @@ function prepareBosh() {
 
   targetBosh
 
-  FOUND_DOCKER_RELEASE=`$BOSH_CMD -e lite releases | grep "docker" | grep $SOLACE_DOCKER_BOSH_VERSION | wc -l`
+  FOUND_DOCKER_RELEASE=`$BOSH_CMD releases | grep "docker" | grep $SOLACE_DOCKER_BOSH_VERSION | wc -l`
   if [ "$FOUND_DOCKER_RELEASE" -eq "0" ]; then
      echo "Uploading docker bosh"
-     $BOSH_CMD -e lite upload-release $SOLACE_DOCKER_BOSH
+     $BOSH_CMD upload-release $SOLACE_DOCKER_BOSH
   else
      echo "$SOLACE_DOCKER_BOSH was found $FOUND_DOCKER_RELEASE"
   fi
 
-  echo "Uploading stemcell"
-
-  if [ ! -f /tmp/$STEMCELL_NAME ]; then
-      wget -O /tmp/$STEMCELL_NAME $STEMCELL_URL
-  fi
-
-  FOUND_STEMCELL=`$BOSH_CMD -e lite stemcells | grep bosh-warden-boshlite-ubuntu-trusty-go_agent | grep $STEMCELL_VERSION | wc -l`
+  FOUND_STEMCELL=`$BOSH_CMD stemcells | grep bosh-warden-boshlite-ubuntu-trusty-go_agent | grep $STEMCELL_VERSION | wc -l`
   if [ "$FOUND_STEMCELL" -eq "0" ]; then
-     $BOSH_CMD -e lite upload-stemcell /tmp/$STEMCELL_NAME
+    if [ ! -f /tmp/$STEMCELL_NAME ]; then
+        wget -O /tmp/$STEMCELL_NAME $STEMCELL_URL
+    fi
+    echo "Uploading stemcell"
+    $BOSH_CMD upload-stemcell /tmp/$STEMCELL_NAME
   else
      echo "$STEMCELL_NAME was found $FOUND_STEMCELL"
   fi
@@ -66,13 +81,13 @@ function prepareBosh() {
 
 function deleteOrphanedDisks() {
 
-$BOSH_CMD -e lite disks --orphaned
+$BOSH_CMD disks --orphaned
 
-ORPHANED_DISKS=$( $BOSH_CMD -e lite disks --orphaned --json | jq '.Tables[].Rows[] | select(.deployment="solace-vmr-warden-deployment") | .disk_cid' | sed 's/\"//g' )
+ORPHANED_DISKS=$( $BOSH_CMD disks --orphaned --json | jq '.Tables[].Rows[] | select(.deployment="solace-vmr-warden-deployment") | .disk_cid' | sed 's/\"//g' )
 
 for DISK_ID in $ORPHANED_DISKS; do
 	echo "Will delete $DISK_ID"
-	$BOSH_CMD -e lite -n delete-disk $DISK_ID
+	$BOSH_CMD -n delete-disk $DISK_ID
 	echo
 	echo "Orphaned Disk $DISK_ID was deleted"
 	echo
@@ -89,23 +104,23 @@ function shutdownVMRJobs() {
  VM_JOB=$1
 
  echo "Looking for VM job $VM_JOB" 
- VM_FOUND_COUNT=`$BOSH_CMD -e lite vms | grep $VM_JOB | wc -l`
- VM_RUNNING_FOUND_COUNT=`$BOSH_CMD -e lite vms --json | jq '.Tables[].Rows[] | select(.process_state=="running") | .instance' | grep $VM_JOB |  wc -l`
- DEPLOYMENT_FOUND_COUNT=`$BOSH_CMD -e lite deployments | grep $DEPLOYMENT_NAME | wc -l`
- RELEASE_FOUND_COUNT=`$BOSH_CMD -e lite releases | grep solace-vmr | wc -l`
+ VM_FOUND_COUNT=`$BOSH_CMD vms | grep $VM_JOB | wc -l`
+ VM_RUNNING_FOUND_COUNT=`$BOSH_CMD vms --json | jq '.Tables[].Rows[] | select(.process_state=="running") | .instance' | grep $VM_JOB |  wc -l`
+ DEPLOYMENT_FOUND_COUNT=`$BOSH_CMD deployments | grep $DEPLOYMENT_NAME | wc -l`
+ RELEASE_FOUND_COUNT=`$BOSH_CMD releases | grep solace-vmr | wc -l`
 
  if [ "$VM_RUNNING_FOUND_COUNT" -eq "1" ]; then
 
    echo "Will stop monit jobs if any are running on $DEPLOYMENT_NAME / $VM_JOB"
-   $BOSH_CMD -e lite -d $DEPLOYMENT_NAME ssh $VM_JOB "sudo /var/vcap/bosh/bin/monit stop all" 
+   $BOSH_CMD ssh $VM_JOB "sudo /var/vcap/bosh/bin/monit stop all" 
 
-   RUNNING_COUNT=`$BOSH_CMD -e lite -d $DEPLOYMENT_NAME ssh $VM_JOB "sudo /var/vcap/bosh/bin/monit summary" | grep running | wc -l`
+   RUNNING_COUNT=`$BOSH_CMD ssh $VM_JOB "sudo /var/vcap/bosh/bin/monit summary" | grep running | wc -l`
    MAX_WAIT=60
    while [ "$RUNNING_COUNT" -gt "0" ] && [ "$MAX_WAIT" -gt "0" ]; do
    	echo "Waiting for monit to finish shutdown - found $RUNNING_COUNT still running"
 	sleep 5
         let MAX_WAIT=MAX_WAIT-5
-        RUNNING_COUNT=`$BOSH_CMD -e lite -d $DEPLOYMENT_NAME ssh $VM_JOB "sudo /var/vcap/bosh/bin/monit summary " | grep running | wc -l`
+        RUNNING_COUNT=`$BOSH_CMD ssh $VM_JOB "sudo /var/vcap/bosh/bin/monit summary " | grep running | wc -l`
    done
  else
   echo "Did not find running job $VM_JOB"
@@ -115,9 +130,9 @@ function shutdownVMRJobs() {
 
 function shutdownAllVMRJobs() {
     local DEPLOYED_MANIFEST="$WORKSPACE/deployed-manifest.yml"
-    $BOSH_CMD -n -e lite -d $DEPLOYMENT_NAME manifest > $DEPLOYED_MANIFEST
+    $BOSH_CMD -n manifest > $DEPLOYED_MANIFEST
     echo "Shutting down all VMR jobs..."
-    VMR_JOBS=$(bosh -e lite -d $DEPLOYMENT_NAME vms --json | jq '.Tables[].Rows[] | select(.process_state=="running") | .instance' | sed 's/\"//g' )
+    VMR_JOBS=$(bosh vms --json | jq '.Tables[].Rows[] | select(.process_state=="running") | .instance' | sed 's/\"//g' )
     for VMR_JOB_NAME in ${VMR_JOBS[@]}; do
         echo "Shutting down $VMR_JOB_NAME"
         shutdownVMRJobs $VMR_JOB_NAME | tee $LOG_FILE
@@ -127,23 +142,23 @@ function shutdownAllVMRJobs() {
 
 function deleteDeploymentAndRelease() {
 
- DEPLOYMENT_FOUND_COUNT=`$BOSH_CMD -e lite deployments | grep $DEPLOYMENT_NAME | wc -l`
- SOLACE_VMR_RELEASE_FOUND_COUNT=`$BOSH_CMD -e lite releases | grep solace-vmr | wc -l`
- SOLACE_MESSAGING_RELEASE_FOUND_COUNT=`$BOSH_CMD -e lite releases | grep solace-messaging | wc -l`
+ DEPLOYMENT_FOUND_COUNT=`$BOSH_CMD deployments | grep $DEPLOYMENT_NAME | wc -l`
+ SOLACE_VMR_RELEASE_FOUND_COUNT=`$BOSH_CMD releases | grep solace-vmr | wc -l`
+ SOLACE_MESSAGING_RELEASE_FOUND_COUNT=`$BOSH_CMD releases | grep solace-messaging | wc -l`
 
  local DEPLOYED_MANIFEST="$WORKSPACE/deployed-manifest.yml"
 
  if [ "$DEPLOYMENT_FOUND_COUNT" -eq "1" ]; then
-    $BOSH_CMD -n -e lite -d $DEPLOYMENT_NAME manifest > $DEPLOYED_MANIFEST
+    $BOSH_CMD -n manifest > $DEPLOYED_MANIFEST
 
     if [ "$USE_ERRANDS" -eq "1" ]; then
       echo "Calling bosh run-errand delete-all"
-      $BOSH_CMD -e lite -d $DEPLOYMENT_NAME run-errand delete-all
+      $BOSH_CMD run-errand delete-all
     fi
 
     # Delete the deployment 
     echo "Deleting deployment $DEPLOYMENT_NAME"
-    $BOSH_CMD -n -e lite -d $DEPLOYMENT_NAME delete-deployment 
+    $BOSH_CMD -n delete-deployment 
  else
    echo "No deployment found."
  fi
@@ -151,7 +166,7 @@ function deleteDeploymentAndRelease() {
  if [ "$SOLACE_VMR_RELEASE_FOUND_COUNT" -eq "1" ]; then
     # solace-vmr
     echo "Deleting release solace-vmr"
-    $BOSH_CMD -n -e lite delete-release solace-vmr
+    $BOSH_CMD -n delete-release solace-vmr
  else
     echo "No solace-vmr release found"
  fi
@@ -159,7 +174,7 @@ function deleteDeploymentAndRelease() {
  if [ "$SOLACE_MESSAGING_RELEASE_FOUND_COUNT" -eq "1" ]; then
     # solace-messaging
     echo "Deleting release solace-messaging"
-    $BOSH_CMD -n -e lite delete-release solace-messaging
+    $BOSH_CMD -n delete-release solace-messaging
  else
     echo "No solace-messaging release found"
  fi
@@ -213,14 +228,14 @@ function uploadAndDeployRelease() {
 echo "in function uploadAndDeployRelease. SOLACE_MESSAGING_BOSH_RELEASE_FILE: $SOLACE_MESSAGING_BOSH_RELEASE_FILE"
 
 SOLACE_MESSAGING_BOSH_RELEASE_FILE=${SOLACE_MESSAGING_BOSH_RELEASE_FILE:-`ls $WORKSPACE/releases/solace-messaging-*.tgz | tail -1`}
-SOLACE_MESSAGING_RELEASE_FOUND_COUNT=`$BOSH_CMD -e lite releases | grep solace-messaging | wc -l`
+SOLACE_MESSAGING_RELEASE_FOUND_COUNT=`$BOSH_CMD releases | grep solace-messaging | wc -l`
 
 if [ -f $SOLACE_MESSAGING_BOSH_RELEASE_FILE ]; then
 
  targetBosh
 
  if [ "$SOLACE_MESSAGING_RELEASE_FOUND_COUNT" -gt "0" ]; then
-  UPLOADED_RELEASE_VERSION=`$BOSH_CMD -e lite releases | grep solace-messaging | awk '{ print $4 }'`
+  UPLOADED_RELEASE_VERSION=`$BOSH_CMD releases | grep solace-messaging | awk '{ print $4 }'`
   # remove trailing '*'
   UPLOADED_RELEASE_VERSION="${UPLOADED_RELEASE_VERSION%\*}"
   echo "Determined solace-messaging uploaded version $UPLOADED_RELEASE_VERSION"
@@ -230,7 +245,7 @@ if [ -f $SOLACE_MESSAGING_BOSH_RELEASE_FILE ]; then
     [ "$SOLACE_MESSAGING_BOSH_RELEASE_VERSION_FULL" '>' "$UPLOADED_RELEASE_VERSION" ]; then
   echo "Will upload release $SOLACE_MESSAGING_BOSH_RELEASE_FILE"
 
-  $BOSH_CMD -e lite upload-release $SOLACE_MESSAGING_BOSH_RELEASE_FILE | tee -a $LOG_FILE
+  $BOSH_CMD upload-release $SOLACE_MESSAGING_BOSH_RELEASE_FILE | tee -a $LOG_FILE
  else
   echo "A solace-messaging release with version greater than or equal to $SOLACE_MESSAGING_BOSH_RELEASE_VERSION_FULL already exists. Skipping release upload..."
  fi
@@ -238,7 +253,7 @@ if [ -f $SOLACE_MESSAGING_BOSH_RELEASE_FILE ]; then
 fi
 
 SOLACE_VMR_BOSH_RELEASE_FILE=${SOLACE_VMR_BOSH_RELEASE_FILE:-`ls $WORKSPACE/releases/solace-vmr-*.tgz | tail -1`}
-RELEASE_FOUND_COUNT=`$BOSH_CMD -e lite releases | grep solace-vmr | wc -l`
+RELEASE_FOUND_COUNT=`$BOSH_CMD releases | grep solace-vmr | wc -l`
 
 echo "in function uploadAndDeployRelease. SOLACE_VMR_BOSH_RELEASE_FILE: $SOLACE_VMR_BOSH_RELEASE_FILE"
 
@@ -247,7 +262,7 @@ if [ -f $SOLACE_VMR_BOSH_RELEASE_FILE ]; then
  targetBosh
 
  if [ "$RELEASE_FOUND_COUNT" -gt "0" ]; then
-  UPLOADED_RELEASE_VERSION=`$BOSH_CMD -e lite releases | grep solace-vmr | awk '{ print $4 }'`
+  UPLOADED_RELEASE_VERSION=`$BOSH_CMD releases | grep solace-vmr | awk '{ print $4 }'`
   # remove trailing '*'
   UPLOADED_RELEASE_VERSION="${UPLOADED_RELEASE_VERSION%\*}"
  fi
@@ -256,7 +271,7 @@ if [ -f $SOLACE_VMR_BOSH_RELEASE_FILE ]; then
     [ "$SOLACE_VMR_BOSH_RELEASE_VERSION_FULL" '>' "$UPLOADED_RELEASE_VERSION" ]; then
   echo "Will upload release $SOLACE_VMR_BOSH_RELEASE_FILE"
 
-  $BOSH_CMD -e lite upload-release $SOLACE_VMR_BOSH_RELEASE_FILE | tee -a $LOG_FILE
+  $BOSH_CMD upload-release $SOLACE_VMR_BOSH_RELEASE_FILE | tee -a $LOG_FILE
  else
   echo "A solace-vmr release with version greater than or equal to $SOLACE_VMR_BOSH_RELEASE_VERSION_FULL already exists. Skipping release upload..."
  fi
@@ -264,7 +279,7 @@ if [ -f $SOLACE_VMR_BOSH_RELEASE_FILE ]; then
  echo "Calling bosh deployment"
  echo "MANIFEST_FILE=$MANIFEST_FILE"
 
-# $BOSH_CMD -e lite deployment $MANIFEST_FILE | tee -a $LOG_FILE 
+# $BOSH_CMD deployment $MANIFEST_FILE | tee -a $LOG_FILE 
 
  VMR_JOBS=$(py "getManifestJobNames" $MANIFEST_FILE)
  for VMR_JOB_NAME in ${VMR_JOBS[@]}; do
@@ -278,26 +293,26 @@ if [ -f $SOLACE_VMR_BOSH_RELEASE_FILE ]; then
     echo "Will deploy VMR with name $VMR_JOB_NAME, having POOL_NAME: $POOL_NAME, and using $SOLACE_DOCKER_IMAGE_NAME" | tee -a $LOG_FILE
  done
 
- $BOSH_CMD -n -e lite -d $DEPLOYMENT_NAME deploy $MANIFEST_FILE | tee -a $LOG_FILE
+ $BOSH_CMD -n deploy $MANIFEST_FILE | tee -a $LOG_FILE
 
 
- $BOSH_CMD -e lite vms
- DEPLOYMENT_FOUND_COUNT=`2>&1 $BOSH_CMD -e lite deployments | grep $DEPLOYMENT_NAME | wc -l`
+ $BOSH_CMD vms
+ DEPLOYMENT_FOUND_COUNT=`2>&1 $BOSH_CMD deployments | grep $DEPLOYMENT_NAME | wc -l`
  if [ "$DEPLOYMENT_FOUND_COUNT" -eq "0" ]; then
    >&2 echo "bosh did not find any deployments - deployment likely failed"
    exit 1
  fi
 
  POOL_NAMES=$(py "getPoolNames")
- FAILED_VMS_COUNT=`2>&1 $BOSH_CMD -e lite vms | grep -E "($(echo ${POOL_NAMES[*]} | tr ' ' '|'))/[0-9]+" | grep -v running | wc -l`
+ FAILED_VMS_COUNT=`2>&1 $BOSH_CMD vms | grep -E "($(echo ${POOL_NAMES[*]} | tr ' ' '|'))/[0-9]+" | grep -v running | wc -l`
  if [ "$FAILED_VMS_COUNT" -gt "0" ]; then
    >&2 echo "Found non-running VMs - deployment likely failed"
    exit 1
  fi
 
  if [ "$USE_ERRANDS" -eq "1" ]; then
-   echo "Calling $BOSH_CMD -e lite run-errand deploy-all"
-   $BOSH_CMD -e lite -d $DEPLOYMENT_NAME run-errand deploy-all
+   echo "Calling $BOSH_CMD run-errand deploy-all"
+   $BOSH_CMD run-errand deploy-all
  fi
 
 else
@@ -330,12 +345,12 @@ function py() {
 
 function runDeleteAllErrand() {
    targetBosh
-   $BOSH_CMD -e lite -d $DEPLOYMENT_NAME run-errand delete-all
+   $BOSH_CMD run-errand delete-all
 }
 
 
 function runDeployAllErrand() {
   targetBosh
-  $BOSH_CMD -e lite -d $DEPLOYMENT_NAME run-errand deploy-all
+  $BOSH_CMD run-errand deploy-all
 }
 
