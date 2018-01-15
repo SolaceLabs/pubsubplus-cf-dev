8343fd21dc821e17f1017b579b09e11116381e8b
Philippe Khalife <philippe.khalife@gmail.com>
2018-01-15 18:17:45 +0000

    Latest changes, using var for 'form equivalent' user input only, start using ops files

--- solace-messaging-deployment/deploy.sh
+++ solace-messaging-deployment/deploy.sh
@@ -2,10 +2,16 @@
 
 # bosh -d solace_messaging run-errand delete-all
 
-bosh -d solace_messaging deploy solace-deployment.yml --vars-store ~/deployment-vars.yml -v system_domain=bosh-lite.com  -l vars.yml
+bosh -d solace_messaging \
+	deploy solace-deployment.yml \
+	-o operations/plan_inventory.yml \
+	-o operations/set_vmr_version.yml 
+	--vars-store ~/deployment-vars.yml \
+	-v system_domain=bosh-lite.com  \
+	-l vars.yml
+
 
 # bosh -d solace_messaging run-errand --keep-alive deploy-all
 
-bosh -d solace_messaging run-errand deploy-all
-
+# bosh -d solace_messaging run-errand deploy-all
 
