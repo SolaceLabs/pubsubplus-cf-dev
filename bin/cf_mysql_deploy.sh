5936a9e421d74fce444eb6e147a85d633d125150
Philippe Khalife <philippe.khalife@gmail.com>
2018-04-18 14:07:00 -0400

    Use -v vars instead of cf_mysql_vars.yml, removed cf_mysql_vars.yml

--- bin/cf_mysql_deploy.sh
+++ bin/cf_mysql_deploy.sh
@@ -7,7 +7,7 @@ export WORKSPACE=${WORKSPACE:-$SCRIPTPATH/../workspace}
 export BOSH_NON_INTERACTIVE=${BOSH_NON_INTERACTIVE:-true}
 
 if [ -f $SCRIPTPATH/cf_env.sh ]; then
- $SCRIPTPATH/cf_env.sh
+ source $SCRIPTPATH/cf_env.sh
 fi
 
 cd $WORKSPACE
@@ -31,8 +31,12 @@ bosh \
   -o $SCRIPTPATH/cf_mysql_add-broker.yml \
   -o operations/register-proxy-route.yml \
   -o operations/latest-versions.yml \
-  -l $SCRIPTPATH/cf_mysql_vars.yml \
-  --vars-store $WORKSPACE/deployment-vars.yml
+  --vars-store $WORKSPACE/deployment-vars.yml \
+  -v cf_mysql_external_host=p-mysql.$SYSTEM_DOMAIN \
+  -v cf_mysql_host=$SYSTEM_DOMAIN \
+  -v cf_api_url=https://$CF_API_DOMAIN \
+  -v cf_skip_ssl_validation=true \
+  -v proxy_vm_extension=mysql-proxy-lb 
 
 [[ $? -eq 0 ]] && {
   bosh -d cf-mysql run-errand broker-registrar-vm
