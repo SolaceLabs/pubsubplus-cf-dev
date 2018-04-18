b7fd30a05b4d5151719a60d1f3846b3636e4c5df
Philippe Khalife <philippe.khalife@gmail.com>
2018-04-18 13:43:51 -0400

    Uses SYSTEM_DOMAIN or CF_API_DOMAIN

--- bin/cf_env.sh
+++ bin/cf_env.sh
@@ -8,5 +8,8 @@ CF_ADMIN_PASSWORD=$(bosh int $WORKSPACE/deployment-vars.yml --path /cf_admin_pas
 export CF_ADMIN_PASSWORD=${CF_ADMIN_PASSWORD:-'admin'}
 export UAA_ADMIN_CLIENT_SECRET=$(bosh int $WORKSPACE/deployment-vars.yml --path /uaa_admin_client_secret)  
 
-cf api https://api.bosh-lite.com --skip-ssl-validation
+SYSTEM_DOMAIN=${SYSTEM_DOMAIN:-"bosh-lite.com"}
+CF_API_DOMAIN=${CF_API_DOMAIN:-"api.$SYSTEM_DOMAIN"}
+
+cf api https://$CF_API_DOMAIN --skip-ssl-validation
 cf auth admin $CF_ADMIN_PASSWORD  
