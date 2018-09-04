3d70bf905199029892ddd7eb4ce835fac96fffad
Michael Cardy <contact@mcardy.com>
2018-09-04 15:31:35 -0400

    Cleanup solace service adapter bosh releases

--- bin/bosh-common.sh
+++ bin/bosh-common.sh
@@ -301,6 +301,15 @@ function deleteSolaceReleases() {
      echo "No solace-pubsub-broker release found: $SOLACE_MESSAGING_RELEASE_FOUND_COUNT"
  fi
 
+SOLACE_SERVICE_ADAPTER_FOUND_COUNT=`bosh releases | grep solace-service-adapter | wc -l`
+if [ "$SOLACE_SERVICE_ADAPTER_FOUND_COUNT" -gt "0" ]; then
+    # solace-service-adapter
+    echo "Deleting release solace-service-adapter"
+    bosh -n delete-release solace-service-adapter
+else
+    echo "No solace-service-adapter release found: $SOLACE_SERVICE_ADAPTER_FOUND_COUNT"
+fi
+
 }
 
 function resume_bosh_lite_vm() {
