5e4af4b52997145beb5ad1f8a2acf6964ee6c2ec
Philippe Khalife <philippe.khalife@gmail.com>
2018-11-05 18:33:32 +0000

    Touch up spacing

--- bin/getServiceBrokerInfo.sh
+++ bin/getServiceBrokerInfo.sh
@@ -6,9 +6,18 @@ export SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $SCRIPTPATH/cf-common.sh
 
 set -e
+
+echo
+
 confirmServiceBrokerRunning
 
 checkServiceBrokerServicePlanStats
 
+echo
+printf "\t\t\t\t\t%s\n" "------------------------------------------------------------------------------------------------------------------------"
+echo
+
 showServiceBrokerVMRs
+
+echo
 
